44b5fa9eb8373d4bdd6d13e0e3aa7496f22c8070
chj <1228200516@qq.com>
2021-06-28 15:30:17 +0800

    add something

--- DataStruct_02/Book_01.c
+++ DataStruct_02/Book_01.c
@@ -5,6 +5,9 @@
 
 int MaxSubsequenceSum_Algorithm_1(const int array[], int n);
 int MaxSubsequenceSum_Algorithm_2(const int array[], int n);
+int MaxSubsequenceSum_Algorithm_3(const int array[], int n);
+static int MaxSubsequenceSum_Algorithm_3_part(const int array[], int left, int right);
+static int FindMAx(int number1, int number2, int number3);
 
 int main(void)
 {
@@ -44,4 +47,37 @@ int MaxSubsequenceSum_Algorithm_2(const int array[], int n)
     }
 
     return MaxSum;
+}
+static int MaxSubsequenceSum_Algorithm_3_part(const int array[], int left, int right)
+{
+    int MaxLeftSum, MaxRightSum;
+    int MaxLeftBoardSum, MaxRightBoardSum;
+    int LeftBoardSum, RightBoardSum;
+    int center;
+
+    if (left == right)
+        if (array[left] > 0)
+            return array[left];
+        else
+            return 0;
+    center = (left + right) / 2;
+    MaxLeftSum = MaxSubsequenceSum_Algorithm_3(array, left, center);
+    MaxRightSum = MaxSubsequenceSum_Algorithm_3(array, center + 1, right);
+
+    MaxLeftBoardSum = 0;
+    LeftBoardSum = 0;
+    for (int i = center; i >= left; --i) {
+        LeftBoardSum += array[i];
+        if (LeftBoardSum > MaxLeftBoardSum)
+            MaxLeftBoardSum = LeftBoardSum;
+    }
+    MaxRightBoardSum = 0;
+    RightBoardSum = 0;
+    for (int i = center + 1; i <= right; ++i)
+    {
+        RightBoardSum += array[i];
+        if (RightBoardSum > MaxRightBoardSum)
+            MaxRightBoardSum = RightBoardSum;
+    }
+
 }
